4c1882517bcde0b8a70735291b32e1de06ed6f77
musaid53 <muhammedcamkerten@gmail.com>
2022-03-13 07:59:42 +0300

    booking manager service developments/learning

--- booking-manager/index.js
+++ booking-manager/index.js
@@ -1,15 +1,15 @@
 const logger = require('koa-logger');
 const router = require('@koa/router')();
 const koaBody = require('koa-body');
-
+const mongoose = require("mongoose");
 const Koa = require('koa');
-const app = new Koa();
+const app = module.exports = new Koa();
 require("./table.model");
-process.env
+const url = process.env.MONGO_URL || "mongodb://localhost:27017/tables"
 
+const Table = mongoose.model("tables");
 app.use(logger());
 app.use(koaBody());
-
 
 
 mongoose
@@ -20,16 +20,17 @@ mongoose
   .then(() => {
     console.log("Connection established ");
   })
-  .catch(() => {
+  .catch((err) => {
     console.log("Connection failed");
+    console.log(err);
   });
 
 
 // route definitions
 
 router.get('/', health)
-    .post('/create-table', createTable)
-    .get('/list-table', listTables);
+  .post('/create-table', createTable);
+//.get('/list-table', listTables);
 
 
 app.use(router.routes());
@@ -39,27 +40,78 @@ app.use(router.routes());
  */
 
 async function health(ctx) {
-    ctx.body = 'Up';;
+  ctx.body = 'Up';
 }
 
 /**
  * Create a table.
  */
 
- async function createTable(ctx) {
-    const post = ctx.request.body;
-    const id = posts.push(post) - 1;
-    post.created_at = new Date();
-    post.id = id;
-    ctx.redirect('/');
+async function createTable(ctx) {
+  const table = new Table(ctx.request.body);
+  // const post = ctx.request.body;
+  
+  table.save()
+  .then((data) => {
+    if(data){
+      ctx.body = data;
+      console.log("table created")
+    }else{
+      console.log("table not  created")
+      //ctx.throw(400);
+    }
+  })
+  .catch((err) => {
+    ctx.throw(400,'Error Message');
+  });
+  
+}
+
+
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    console.log('asdadasdas');
+    // will only respond with JSON
+    ctx.status = err.statusCode || err.status || 500;
+    ctx.body = {
+      message: err.message
+    };
   }
-
-
-
+})
 
 
 // app.use(async ctx => {
 //     ctx.body = 'Up';
 // });
+/*
+app.use(async function(ctx, next) {
+  try {
+    await next();
+  } catch (err) {
+    console.log('said');
+    // some errors will have .status
+    // however this is not a guarantee
+    ctx.status = err.status || 500;
+    ctx.type = 'html';
+    ctx.body = '<p>Something <em>exploded</em>, please contact Maru.</p>';
 
+    // since we handled this manually we'll
+    // want to delegate to the regular app
+    // level error handling as well so that
+    // centralized still functions correctly.
+    ctx.app.emit('error', err, ctx);
+  }
+});
+
+
+app.on('error', function(err) {
+  console.log('err');
+  if (process.env.NODE_ENV != 'test') {
+    console.log('sent error %s to the cloud', err.message);
+    console.log(err);
+  }
+});
+*/
 app.listen(3000);
--- booking-manager/table.model.js
+++ booking-manager/table.model.js
@@ -1,8 +1,29 @@
 const mongoose = require('mongoose');
 
+var tableSchema = mongoose.Schema({
+    _id: {type: String, required: true}
+    // seq: { type: Number, default: 0 }
+});
+
+
+tableSchema.post('save', function(error, doc, next) {
+    if (error.name === 'MongoError' && error.code === 11000) {
+      //next(new Error('There was a duplicate key error'));
+        console.error('There was a duplicate key error');
+    } else {
+      next(error);
+    }
+  });
+
+mongoose.model('tables', tableSchema);
+
+
+
+/*
 mongoose.model('tables',{
     tableId: {
         type: mongoose.SchemaTypes.ObjectId,
         required: true 
     }
 })
+*/
